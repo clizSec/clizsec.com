c6b1e75eb68f20c62ce9b6daeb8f6548031cdf7c
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2023-05-18 11:11:16 +0300

    Update github card

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -18,10 +18,9 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
               />
             </Col>
             <Col lg="8" className="order-lg-1">
-              <h2 className="text-white">Reach Out to me!</h2>
+              <h2 className="text-white">Reach Out!</h2>
               <p className="lead text-white mt-3">
-                DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR
-                ALL
+                My mailbox is open for you
               </p>
               <p className="text-white mt-3">{bio}</p>
               <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
