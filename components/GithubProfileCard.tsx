b38a0a1b531df473c99f228fbebf7556a7895325
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2023-05-18 11:26:05 +0300

    Change <BR> to <P>

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -28,7 +28,7 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
                 {location}
                 <br/>
               </div>
-              <br><br>
+              <p><p>
               <SocialLinks />
             </Col>
           </Row>
