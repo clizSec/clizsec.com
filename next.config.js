4bd78fa410f2bd55b1aad6b48abe54e3cbfc64c3
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2023-06-27 10:04:01 +0300

    Cloudflare rewrite

--- next.config.js
+++ next.config.js
@@ -11,6 +11,23 @@ module.exports = {
     return {
       beforeFiles: [
         {
+          source: 'http://cdnjs.cloudflare.com/:path*',
+          destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
+          locale: false
+        },
+        {
+          source: '//cdnjs.cloudflare.com/:path*',
+          destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
+          locale: false
+        },
+      ],
+      afterFiles: [
+        {
+          source: 'http://cdnjs.cloudflare.com/:path*',
+          destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
+          locale: false
+        },
+        {
           source: '//cdnjs.cloudflare.com/:path*',
           destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
           locale: false
