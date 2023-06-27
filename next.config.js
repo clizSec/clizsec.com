77fd1e9fd4ab706580d867e554f7b2d9bb5326cb
Mark Liapustin (clizSec) <mliapustin@trustificorp.com>
2023-06-27 10:18:56 +0300

    Force HTTPS

--- next.config.js
+++ next.config.js
@@ -11,22 +11,12 @@ module.exports = {
     return {
       beforeFiles: [
         {
-          source: 'http://cdnjs.cloudflare.com/:path*',
-          destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
-          locale: false
-        },
-        {
           source: '//cdnjs.cloudflare.com/:path*',
           destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
           locale: false
         },
       ],
       afterFiles: [
-        {
-          source: 'http://cdnjs.cloudflare.com/:path*',
-          destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
-          locale: false
-        },
         {
           source: '//cdnjs.cloudflare.com/:path*',
           destination: 'https://cdnjs.cloudflare.com/:path*', // Force HTTPs
